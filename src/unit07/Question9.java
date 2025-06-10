80d79afbf559fbdb866614cef5ba671920d39db7
Aiden Castillo <40736622@live.napier.ac.uk>
2025-06-10 10:02:17 -0600

    Used rectangle methods to get rid of warning

--- src/unit07/Question9.java
+++ src/unit07/Question9.java
@@ -11,6 +11,12 @@ public class Question9 {
 		Rectangle rec4 = new Rectangle(26, 23);
 		Rectangle rec5 = new Rectangle();
 		
+		rec.increaseBy(10);
+		rec2.increaseBy(10);
+		rec3.increaseBy(10);
+		rec4.increaseBy(10);
+		rec5.increaseBy(10);
+		
 		String output = String.format("Rectangles created: %d", Rectangle.getNumOfInstances());
 		JOptionPane.showMessageDialog(null, output, "Rectangle", JOptionPane.PLAIN_MESSAGE);
 	}
